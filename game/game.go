2360ac1f971666f2eaf4de7c3408b47fd444cbe7
Loowootoo <soabw9721@gmail.com>
2018-10-16 19:13:24 +0800

    ...

--- game/game.go
+++ game/game.go
@@ -1,15 +1,17 @@
 package game
 
 type Game struct {
-	Player  *Player
-	CardSet *Cards
-	Message string
+	Player        *Player
+	CardSet       *Cards
+	Message       string
+	GameStatus    MainGameState
+	GameSubStatus int
 }
 
 func NewGame(credit int) *Game {
 	player := NewPlayer(credit)
 	cardSet := NewCardSet()
-	return &Game{player, cardSet, ""}
+	return &Game{player, cardSet, "", GameDEMO, 0}
 }
 
 func (g *Game) AddBet(bet int) {
@@ -38,7 +40,6 @@ var testCount float64
 func (g *Game) Run() {
 	testCount++
 	if testCount > 60 {
-		g.Shuffle()
 		g.Deal()
 		g.Message = g.Player.CheckWin()
 		testCount = 0
@@ -50,6 +51,7 @@ type MainGameState int
 const (
 	GameDEMO MainGameState = iota
 	GameSTART
+	GameBET
 	GamePLAY
 	GameCHECK
 	GameWIN
@@ -60,9 +62,26 @@ const (
 var GameMessage = [...]string{
 	"GAME DEMO",
 	"GAME START",
+	"GAME BET",
 	"GAME PLAY",
 	"GAME CHECK",
 	"GAME WIN",
 	"GAME LOSE",
 	"GAME ACCOUNT",
+}
+
+func (g *Game) GameLoop() {
+	switch g.GameStatus {
+	case GameDEMO:
+		g.GameStatus = GameSTART
+	case GameSTART:
+		g.GameStatus = GameBET
+	case GameBET:
+		g.GameStatus = GamePLAY
+	case GamePLAY:
+	case GameCHECK:
+	case GameWIN:
+	case GameLOSE:
+	case GameACCOUNT:
+	}
 }
